7998aec1c60ea3534c4b81a8c45f16ed6eca3366
=DerasChris <=deras.christian011@gmail.com>
2024-09-14 19:53:00 -0600

    Agregando fixes barra  de navegación

--- lib/navegacion.dart
+++ lib/navegacion.dart
@@ -24,28 +24,57 @@ class _NavigationState extends State<Navigation> {
         ModuloInicio(),
 
   ];
+
+  // Método para generar íconos dinámicamente con colores dependiendo del estado
+  Widget buildIcon(IconData iconData, int itemIndex) {
+    return Icon(
+      iconData,
+      size: 30,
+      color: index == itemIndex ? Colors.white : Colors.black, // Cambia el color según el ítem seleccionado
+    );
+  }
   @override
   Widget build(BuildContext context) {
     final items = <Widget>[
-      Icon(Icons.home,size: 30,),
-      Icon(Icons.search,size: 30,),
-      Icon(Icons.add,size: 30,),
-      Icon(Icons.settings,size: 30,),
-      Icon(Icons.person,size: 30,),
+      buildIcon(Icons.home, 0),
+      buildIcon(Icons.search, 1),
+      buildIcon(Icons.add, 2),
+      buildIcon(Icons.settings, 3),
+      buildIcon(Icons.person, 4),
     ];
-    return Scaffold(
-      body: Screens[index],
-      bottomNavigationBar: Theme(
-        data: Theme.of(context).copyWith(
-          iconTheme: const IconThemeData(color: Colors.black)
-        ),
-        child: CurvedNavigationBar(
-          color: Color.fromARGB(255, 214, 214, 214),
-          key: navigationKey,
-          items: items,
-          index: index,
-          onTap: (index )=> setState (() => this.index = index),
-          backgroundColor: Colors.transparent ,
+    return SafeArea(
+      top: false,
+      child: Scaffold(
+        extendBody: true,
+        body: Screens[index],
+        bottomNavigationBar: Theme(
+          data: Theme.of(context).copyWith(
+            iconTheme: const IconThemeData(color: Colors.black)
+          ),
+          child: Container(
+            decoration: BoxDecoration(
+                color: Colors.transparent,
+                borderRadius: BorderRadius.circular(45), // Aplica los bordes redondeados
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.3),
+                    spreadRadius: 1,
+                    blurRadius: 10,
+                    offset: Offset(0, 3), // sombra hacia abajo
+                  ),
+                ],
+              ),
+            child: CurvedNavigationBar(
+              height: 50,
+              color: const Color.fromARGB(255, 255, 193, 7),
+              key: navigationKey,
+              items: items,
+              index: index,
+              onTap: (index )=> setState (() => this.index = index),
+              backgroundColor: Colors.transparent ,
+              buttonBackgroundColor: const Color.fromARGB(255, 0, 51, 102),
+            ),
+          ),
         ),
       ),
     );
