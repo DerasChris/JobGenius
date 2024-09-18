8310dbaeba87853fab805972eb3f0124884a4d18
MCoreasDJes <coreaselizabeth2001@gmail.com>
2024-09-17 21:51:57 -0600

    agregando el primer avance de a pantalla, container blanco

---
+++ lib/detalleTrabajador.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'theme/app_color.dart';
+
+import 'package:jobjenius/widgets/CatWidget.dart';
+
+
+class DetalleTrabajador extends StatefulWidget {
+	const DetalleTrabajador({super.key});
+
+	@override
+  State<DetalleTrabajador> createState() => _DetalleTrabajador();
+
+}
+
+void main() {
+  runApp(const DetalleTrabajador());
+}
+
+class _DetalleTrabajador extends State<DetalleTrabajador>{
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: appColor.azul,
+        body: SingleChildScrollView(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Padding(
+                    padding: EdgeInsets.only(top: 80, left: 25, right: 25),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Icon(Icons.arrow_back, color: Colors.white,),
+                              Text(
+                                'Detalles del Trabajador',
+                                style: TextStyle( color:Colors.white)
+                              ),
+                            ],
+                        ),
+                      ],
+                    ),
+                ),
+                Container(
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(50.0),
+                        topRight: Radius.circular(50.0),
+                      )
+                    ), 
+                    //height: 100, 
+                    child: Column(
+                      children: [
+                        Text("Contactar"),
+                        Container(
+                          child: CategoriaWidget(),
+                        ),
+                      ]
+                    )
+                ),
+              ],
+          ),
+        ),
+      ),
+    );    
+  }
+}
