b536e738ee476f923e84bfe65e37a9b8232da930
=DerasChris <=deras.christian011@gmail.com>
2024-11-03 19:47:10 -0600

    Hot fix arreglando problemas de integración

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:jobjenius/theme/app_color.dart';
 import 'package:simple_animation_transition/simple_animation_transition.dart';
 import 'utils/utils.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'logg.dart'; // Asegúrate de importar la página principal
+import 'logg.dart'; 
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 void main() {
